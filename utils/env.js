ede5f01baaa322210a4c44a6ffde388f5288f827
= <sriramr083@gmail.com>
2018-11-30 00:38:10 +0530

    added Dockerfile and docker-compose.yml

--- utils/env.js
+++ utils/env.js
@@ -1,7 +1,7 @@
 // this module reads all Environmental Variables and sets a default value for testing
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/socialcops",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://socialmongo:27017/socialcops",
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || "somesecretkeythatyouwillneverfigureout",
   SENTRY_DSN: process.env.SENTRY_DSN,
   NODE_ENV: process.env.NODE_ENV || 'dev'
