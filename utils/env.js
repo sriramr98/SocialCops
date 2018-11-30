3353d8127e12f8e8356af433cd074827c129fc1c
= <sriramr083@gmail.com>
2018-11-30 20:29:25 +0530

    small bug fix

--- utils/env.js
+++ utils/env.js
@@ -5,10 +5,10 @@
 
 module.exports = {
     PORT: process.env.PORT || 3000,
-    MONGODB_URI: process.env.MONGODB_URI ||
-    'mongodb://socialmongo:27017/socialcops',
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY ||
-    'somesecretkeythatyouwillneverfigureout',
+    MONGODB_URI:
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialcops',
+    JWT_SECRET_KEY:
+    process.env.JWT_SECRET_KEY || 'somesecretkeythatyouwillneverfigureout',
     SENTRY_DSN: process.env.SENTRY_DSN,
-    NODE_ENV: process.env.NODE_ENV || 'dev',
+    NODE_ENV: process.env.NODE_ENV || 'dev'
 };
